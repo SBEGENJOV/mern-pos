20bcd4221da8bb91c1142ce1f5e89b14680df359
SBEGENJOV <seydi19981995@gmail.com>
2024-04-02 06:50:47 +0300

    add/cart component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
+import Categosires from "./component/cateories/Categosires";
 import Header from "./component/header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      home
+    <>
+    <Header />
+    <div className="home px-6 flex justify-between gap-10">
+      <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-103px)] pb-64">
+        <Categosires />
+      </div>
+      <div className="products flex-[8]">
+        <div className="">products</div>
+      </div>
+      <div className="">
+        <div>cart totals</div>
+      </div>
     </div>
+  </>
   );
 }
 
---
+++ client/src/component/cateories/Categosires.jsx
@@ -0,0 +1,38 @@
+const Categosires = () => {
+  return (
+    <ul className="flex gap-4 flex-col text-lg ">
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+        <span className="bold">Tümü</span>
+      </li>
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+        <span className="bold">Yiyecek</span>
+      </li>
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+        <span className="bold">İçecek</span>
+      </li>
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+        <span className="bold">İçecek</span>
+      </li>
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+        <span className="bold">İçecek</span>
+      </li>
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+        <span className="bold">İçecek</span>
+      </li>
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+        <span className="bold">İçecek</span>
+      </li>
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+        <span className="bold">İçecek</span>
+      </li>
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+        <span className="bold">İçecek</span>
+      </li>
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+        <span className="bold">İçecek</span>
+      </li>
+    </ul>
+  );
+};
+
+export default Categosires;
