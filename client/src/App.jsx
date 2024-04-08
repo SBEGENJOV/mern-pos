1453172717c60909ffd17ca95d5b96a1fc4245ff
SBEGENJOV <seydi19981995@gmail.com>
2024-04-08 18:52:22 +0300

    update/ responsive settings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,16 +7,16 @@ function App() {
   return (
     <>
       <Header />
-      <div className="home px-6 flex justify-between gap-10">
-        <div className="categories  overflow-auto max-h-[calc(100vh-_-103px)] pb-64">
+      <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24">
+        <div className="categories  overflow-auto max-h-[calc(100vh_-_103px)] md:pb-10 md:mr-0 -mr-[20px] ">
           <Categosires />
         </div>
-        <div className="products flex-[8]">
+        <div className="products flex-[8] overflow-auto max-h-[calc(100vh_-_103px)]">
           <div className="">
             <Products />
           </div>
         </div>
-        <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
+        <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border ">
           <CartTotals />
         </div>
       </div>
--- client/src/component/cateories/Categosires.jsx
+++ client/src/component/cateories/Categosires.jsx
@@ -1,6 +1,6 @@
 const Categosires = () => {
   return (
-    <ul className="flex gap-4 flex-col text-lg ">
+    <ul className="flex gap-4 md:flex-col  text-lg ">
       <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center min-w-[145px]">
         <span className="bold">Tümü</span>
       </li>
