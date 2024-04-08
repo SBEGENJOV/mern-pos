e67ab41478b740e562d29d2c5165de81063a1140
SBEGENJOV <seydi19981995@gmail.com>
2024-04-08 17:40:20 +0300

    add/cart totals component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import CartTotals from "./component/cart/CartTotals";
 import Categosires from "./component/cateories/Categosires";
 import Header from "./component/header/Header";
 import Products from "./component/product/Products";
@@ -7,7 +8,7 @@ function App() {
     <>
       <Header />
       <div className="home px-6 flex justify-between gap-10">
-        <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-103px)] pb-64">
+        <div className="categories  overflow-auto max-h-[calc(100vh-_-103px)] pb-64">
           <Categosires />
         </div>
         <div className="products flex-[8]">
@@ -15,8 +16,8 @@ function App() {
             <Products />
           </div>
         </div>
-        <div className="">
-          <div>cart totals</div>
+        <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
+          <CartTotals />
         </div>
       </div>
     </>
--- client/src/component/cateories/Categosires.jsx
+++ client/src/component/cateories/Categosires.jsx
@@ -1,34 +1,34 @@
 const Categosires = () => {
   return (
     <ul className="flex gap-4 flex-col text-lg ">
-      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center min-w-[145px]">
         <span className="bold">Tümü</span>
       </li>
-      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center min-w-[145px]">
         <span className="bold">Yiyecek</span>
       </li>
-      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center min-w-[145px]">
         <span className="bold">İçecek</span>
       </li>
-      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center min-w-[145px]">
         <span className="bold">İçecek</span>
       </li>
-      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center min-w-[145px]">
         <span className="bold">İçecek</span>
       </li>
-      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center min-w-[145px]">
         <span className="bold">İçecek</span>
       </li>
-      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center min-w-[145px]">
         <span className="bold">İçecek</span>
       </li>
-      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center min-w-[145px]">
         <span className="bold">İçecek</span>
       </li>
-      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center min-w-[145px]">
         <span className="bold">İçecek</span>
       </li>
-      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center">
+      <li className="bg-blue-700 px-6 py-10 text-white  cursor-pointer hover:bg-blue-900 text-center min-w-[145px]">
         <span className="bold">İçecek</span>
       </li>
     </ul>
