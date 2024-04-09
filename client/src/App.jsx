c7382e59d6d13076b7ecaecb69bb1e65e8abd72d
SBEGENJOV <seydi19981995@gmail.com>
2024-04-09 11:13:28 +0300

    add/bill page and responsive desing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
+import BillPage from "./pages/BillPage";
+import Customers from "./pages/CustomersPage";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/bills" element={<BillPage />} />
+        <Route path="/customers" element={<Customers />} />
       </Routes>
     </BrowserRouter>
   );
