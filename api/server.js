90806f69d3570c1f11f98ea0f83b5d9d5273ea86
SBEGENJOV <seydi19981995@gmail.com>
2024-04-11 18:13:04 +0300

    add/ mongo db, env and express. Create category model

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const port = 5000;
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Baglantı gerçekleşti");
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.listen(port, () => {
+  connect();
+  console.log(`sunucu şuan ${port} unda çalışıyor`);
+});
