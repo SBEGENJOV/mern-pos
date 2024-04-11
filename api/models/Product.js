19d97e72dba2c26dfa9a6311008dfaff9218ea33
SBEGENJOV <seydi19981995@gmail.com>
2024-04-11 18:22:03 +0300

    add/product model

--- api/models/Product.js
+++ api/models/Product.js
@@ -1,1 +1,18 @@
+const mongoose = require("mongoose");
 
+const ProductSchema = mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    img: { type: String, require: true },
+    price: { type: Number, require: true },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "categories",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("products", ProductSchema);
+module.exports = Product;
